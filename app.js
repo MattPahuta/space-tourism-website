64a1eaa17b0f64ad97ab39d4957b28f98526acde
Matt Pahuta <mhpahuta@gmail.com>
2021-11-19 11:55:38 -0800

    Add mobile nav functionality, add header styles

---
+++ app.js
@@ -0,0 +1,20 @@
+// *** Mobile navigation functionality *** //
+
+const nav = document.querySelector('.primary-navigation');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+
+// listen for click event on navToggle
+navToggle.addEventListener('click', () => {
+
+  const visibility = nav.getAttribute('data-visible');
+
+  if (visibility === 'false') {
+    nav.setAttribute('data-visible', true);
+    navToggle.setAttribute('aria-expanded', true);
+  } else {
+    nav.setAttribute('data-visible', false);
+    navToggle.setAttribute('aria-expanded', false);
+  }
+
+});
+
