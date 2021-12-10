d5faee2c257c49cd72ec65d14d5c1910079ebab6
Matt Pahuta <mhpahuta@gmail.com>
2021-12-10 14:37:52 -0800

    Complete destination page content, tabs, interactivity

--- app.js
+++ app.js
@@ -19,15 +19,86 @@ navToggle.addEventListener('click', () => {
 });
 
 // *** Tabs behavior *** //
+// ********************* //
+// split into seperate JS file?
 const tabList = document.querySelector('[role="tablist"]');
-const tabs = document.querySelectorAll('[role="tab"]');
+const tabs = document.querySelectorAll('[role="tab"]'); // node list
+let tabFocus = 0;
 
-tabList.addEventListener('keydown', (e) => {
-  const keydownLeft = 37;
-  const keydownRight = 39;
+tabList.addEventListener('keydown', updateTabFocus)
 
-  // change 
-
-
-  console.log(e.keyCode)     
+tabs.forEach(tab => {
+  tab.addEventListener('click', updateTabPanel);
 })
+
+function updateTabFocus(e) {
+  const keydownLeft = 37; // ArrowLeft keyCode
+  const keydownRight = 39; // ArrowRight keyCode
+
+  if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
+    tabs[tabFocus].setAttribute('tabindex', -1);
+    
+    if (e.keyCode === keydownRight) {
+      tabFocus += 1;
+      if (tabFocus >= tabs.length) {
+        tabFocus = 0;
+      }
+    } else if (e.keyCode === keydownLeft) {
+      tabFocus -= 1;
+      if (tabFocus < 0) {
+        tabFocus = tabs.length - 1;
+      }
+    }
+  
+    tabs[tabFocus].setAttribute("tabindex", 0);
+    tabs[tabFocus].focus();
+  }
+}
+
+
+// show/hide destination info panels
+function updateTabPanel(e) {
+  const targetTab = e.target;
+  const targetPanel = targetTab.getAttribute("aria-controls");
+  const targetPic = targetTab.getAttribute("data-image");
+  
+  const tabContainer = targetTab.parentNode;
+  const mainContainer = tabContainer.parentNode;
+
+  // update active/selected states of buttons
+  tabContainer.querySelector('[aria-selected="true"]').setAttribute("aria-selected", false);
+  targetTab.setAttribute("aria-selected", true);
+
+  // mainContainer
+  //   .querySelectorAll('[role="tabpanel"]')
+  //   .forEach((panel) => panel.setAttribute('hidden', true));
+  // refactor of above:
+  hideContent(mainContainer, '[role="tabpanel"]')
+  // mainContainer.querySelector([`#${targetPanel}`]).removeAttribute('hidden');
+  // refactor of above:
+  showContent(mainContainer, [`#${targetPanel}`])
+
+  hideContent(mainContainer, 'picture')
+  // update image
+  // mainContainer
+  // .querySelectorAll('picture')
+  // .forEach((pic) => pic.setAttribute('hidden', true));
+
+  // mainContainer.querySelector([`#${targetPic}`]).removeAttribute('hidden');
+  // refactor of above:
+  showContent(mainContainer, [`#${targetPic}`])
+
+}
+
+function hideContent(parent, content) {
+  parent
+    .querySelectorAll(content)
+    .forEach((item) => item.setAttribute('hidden', true));
+}
+
+function showContent(parent, content) {
+  parent
+    .querySelector(content)
+    .removeAttribute('hidden');
+}
+
